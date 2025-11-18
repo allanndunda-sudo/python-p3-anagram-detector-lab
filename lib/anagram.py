648a306eccb4b1c04e7e0dec8d3c0fc77d8db9ff
allanndunda <allanndunda>
2025-11-18 08:48:49 +0300

    Pushed work

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,19 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+        self.sorted_word = sorted(self.word)
+
+    def match(self, words):
+        matches = []
+
+        for candidate in words:
+            candidate_lower = candidate.lower()
+
+            
+            if candidate_lower == self.word:
+                continue
+
+            if sorted(candidate_lower) == self.sorted_word:
+                matches.append(candidate)
+
+        return matches
